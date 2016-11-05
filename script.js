4a529ab26d3d2135b930c219a898e6b1d599c76f
skpartha1 <skpartha1@gmail.com>
2016-11-04 21:08:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+(function (window){
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+console.log(names)
+for (var i = 0; i < names.length; i++) {
+  
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+  console.log(firstLetter)
+  
+    
+  if (firstLetter == 'j')
+  {
+       byeSpeaker.speak(names[i]);
+  }
+ else {
+        helloSpeaker.speak(names[i]);
+  }
+}
+
+})(window);
